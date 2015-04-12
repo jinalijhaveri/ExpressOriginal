15213a3ab7ea0fce0dd33056a1ca572cead34290
snketdesai <snket.desai@gmail.com>
2015-04-12 04:14:32 -0700

    all

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var profile = require('./routes/profile');
 var user = require('./routes/users');
 var jobapp = require('./routes/jobapplication');
 var companyprofile = require('./routes/companyprofile');
-//var job = require('./routes/jobs');
+var job = require('./routes/jobs');
 
 var bodyParser = require('body-parser');
 var fs = require("fs");
@@ -78,7 +78,7 @@ app.get('/userapplication/:userId', jobapp.getJobApplication);
 app.post('/updatejobstatus/:jobId/:userId', jobapp.updateJobStatus);
 app.get('/getName/:userId', user.getName);
 
-/*app.get('/insertJobDetailsPage',job.showInsertJobDetailsView);
+app.get('/insertJobDetailsPage',job.showInsertJobDetailsView);
 app.get('/showJobDetailsPage/:jobId',job.showJobDetailsView);
 app.get('/showJobs',job.showJobsView);
 
@@ -86,7 +86,7 @@ app.get('/jobs',job.getJobs);
 app.get('/jobs/:jobId',job.getJobDetails);
 app.get('/searchJobs/:searchTerm',job.searchJobs);
 app.get('/company/:companyId/jobs',job.getJobsByCompany);
-app.post('/company/:companyId/jobs/',job.insertJobDetails);*/
+app.post('/company/:companyId/jobs/',job.insertJobDetails);
 
 app.get('/searchpage', companyprofile.getSearchView);
 app.get('/companyregistrationpage', companyprofile.getCompanyRegisterView);
